7b6561d899662fac0db22868ea64f1dfc3c6848a
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-03-09 16:12:28 -0500

    Atomiza las funciones y hace un sólo llamado a regex para mejorar rendimiento

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,18 +1,18 @@
 const exp = {
-    project: /@[a-z0-9-]+/i,
-    category: /#[a-z0-9-]+/gi,
+    project: /@[a-zA-ZÀ-ÿ]+/,
+    category: /#[a-zA-ZÀ-ÿ]+/g,
 
     // prettier-ignore
     es: {
         relevance: /[*!]\B|(importante|urgente)\b/gi,
         timer: /(t:|tengo) ?([0-9.,]+) ?(minutos?|min|m|horas?|h)\b/i,
-	loopAbsolute: /todos los (lunes|martes|miercoles|jueves|viernes|sabado|domingo|[0-9]{1,2})/i,
+	loopAbsolute: /todos los (lunes|martes|mi.rcoles|jueves|viernes|s.bado|domingo|[0-9]{1,2})/i,
 	loopRelative: /cada ([0-9]{1,2}) (dias|semanas|meses)?/i,
 	loopCount: /[0-9] veces/i,
 
         // WARN: revisar si la abreviación de los meses puede causar algún error
         date1: /([0-9]{1,2})(\/[0-9]{1,2}| de [ad-fjm-os][a-jl-vy-z]{3,9})/i,
-        date2: /(dentro de |de (hoy|ma.ana|este (lunes|martes|miercoles|jueves|viernes|sabado|domingo)) en )([0-9]+ )(dia.|semana.)?/i,
+        date2: /(dentro de |de (hoy|ma.ana|este (lunes|martes|miercoles|jueves|viernes|sabado|domingo)) en |en )([0-9]+ )(dia.|semana.)/i,
 	date3: /(el |este |el proximo )(lunes|martes|miercoles|jueves|viernes|sabado|domingo)/i,
         date4: /(pasado )?(hoy |ma.ana )/i,
     },
--- src/lib/index.js
+++ src/lib/index.js
@@ -2,163 +2,98 @@
 // import './styles.css'; // estilos del proyecto
 // import { use } from 'browser-sync';
 import { exp, daysWeek, months } from './data.js';
-import { timeToSeconds } from './misc.js';
+// import { timeToSeconds } from './misc.js';
+
+// TODO: obtener las fechas relativas en función de la fecha seteada
+// realizar el conteo de loops
+// desarrollar interfase
+// integrar con ls interfase
 
 class Task {
     constructor(prompt) {
         this.parseTask(prompt);
+        // this.taskCreation = new Date();
+        this.done = false;
     }
 
     parseTask(prompt) {
         this.originalPrompt = prompt;
-        this.prompt = prompt
+        const taskParse = prompt
             .replace(/\s{1,}/g, ' ')
             .toLowerCase()
             .replace(/á|é|í|ó|ú|ü/g, (match) => {
-                if (/á/.test(match)) return 'a';
-                if (/é/.test(match)) return 'e';
-                if (/í/.test(match)) return 'i';
-                if (/ó/.test(match)) return 'o';
-                if (/ú|ü/.test(match)) return 'u';
+                if (match === 'á') return 'a';
+                if (match === 'é') return 'e';
+                if (match === 'í') return 'i';
+                if (match === 'ó') return 'o';
+                if (match === 'ú' || match === 'ü') return 'u';
             });
-        // NOTE: función para remover acentos del prompt
-        // string.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-        // descartada porque también afectaba la ñ
-        this.userTask = '';
+        // this.task = '';
 
-        this.project = exp.project.test(this.prompt)
-            ? this.prompt.match(exp.project)[0]
-            : '';
+        // Task definitions, RegexResults. Project an categoty are case sensitive
+        const projectDef = prompt.match(exp.project);
+        const categoryDef = prompt.match(exp.category);
+        const relevanceDef = taskParse.match(exp.es.relevance);
+        const timerDef = taskParse.match(exp.es.timer);
+        const recurrentAbsDef = taskParse.match(exp.es.loopAbsolute);
+        const recurrentRelDef = taskParse.match(exp.es.loopRelative);
+        const recurrentLoopCountRR = taskParse.match(exp.es.loopCount);
+        const date1RR = taskParse.match(exp.es.date1);
+        const date2RR = taskParse.match(exp.es.date2);
+        const date3RR = taskParse.match(exp.es.date3);
+        const date4RR = taskParse.match(exp.es.date4);
 
-        this.category = exp.category.test(this.prompt)
-            ? this.prompt.match(exp.category).slice(0, 2)
-            : '';
-
-        if (exp.es.relevance.test(this.prompt)) {
-            this.important = /\*|^i/i.test(
-                this.prompt.match(exp.es.relevance).pop(),
-            );
-            this.urgent = !this.important;
-        } else {
-            this.important = false;
-            this.urgent = false;
-        }
-
-        this.timer = exp.es.timer.test(this.prompt)
-            ? timeToSeconds(
-                  this.prompt.match(exp.es.timer)[2],
-                  this.prompt.match(exp.es.timer)[3],
-              )
-            : false;
-        this.timerPast = this.timer ? 0 : false;
-
-        this.recurrent =
-            exp.es.loopAbsolute.test(this.prompt) ||
-            exp.es.loopRelative.test(this.prompt);
-
-        this.dueDate = this.nextRecurrentDate() || this.getFixedDate();
+        // Set task object properties
+        if (projectDef) this.project = projectDef[0];
+        if (categoryDef) this.categories = categoryDef.slice(0, 2);
+        if (relevanceDef) this.setRelevance(relevanceDef);
+        if (timerDef) this.setTimer(timerDef);
+        if (recurrentAbsDef) this.setNextRecurrentDate(recurrentAbsDef);
+        if (!this.dueDate && date1RR) this.setFixedDate1(date1RR);
+        if (!this.dueDate && date2RR) this.setFixedDate2(date2RR);
+        if (!this.dueDate && date3RR) this.setFixedDate3(date3RR);
+        if (!this.dueDate && date4RR) this.setFixedDate4(date4RR);
     }
 
-    getFixedDate() {
-        const dueDate = new Date();
-
-        if (this.prompt.match(exp.es.date1)) {
-            const day = +this.prompt.match(exp.es.date1)[1];
-            let month = this.prompt
-                .match(exp.es.date1)[2]
-                .replace(/ de |\//g, '');
-
-            month = /\d/.test(month)
-                ? +month - 1
-                : this.getIndex(months.es, month);
-            dueDate.setDate(day);
-            dueDate.setMonth(month);
-            if (new Date() > dueDate)
-                dueDate.setFullYear(dueDate.getFullYear() + 1);
-
-            return dueDate;
-        }
-
-        if (this.prompt.match(exp.es.date2)) {
-            const day = this.prompt.match(exp.es.date2);
-            console.log(day);
-
-            let daysBetween = day[5] ?? 'dia';
-            if (daysBetween.slice(0, 3) === 'dia') daysBetween = '1';
-            if (daysBetween.slice(0, 3) === 'sem') daysBetween = '7';
-            daysBetween = +daysBetween * +day[0].match(/\d+/);
-            if (!/sem/i.test(day[0])) daysBetween -= 1 // NOTE: quita el día que las personas no tienen en cuenta
-            if (/ma.ana/i.test(day[0])) daysBetween += + 1;
-
-            // Calcular el día de inicio del conteo
-            if (/de e/i.test(day)) {
-                const dayIndex = this.getIndex(daysWeek.es, day[3]);
-                dueDate.setDate(
-                    dayIndex < dueDate.getDay()
-                        ? dueDate.getDate() + dayIndex + (7 - dueDate.getDay())
-                        : dueDate.getDate() + (dayIndex - dueDate.getDay()),
-                );
-            }
-            dueDate.setDate(dueDate.getDate() + daysBetween);
-
-            return dueDate;
-        }
-
-        if (this.prompt.match(exp.es.date3)) {
-            const day = this.prompt.match(exp.es.date3);
-            const dayIndex = this.getIndex(daysWeek.es, day[2]);
-
-            dueDate.setDate(
-                dayIndex < dueDate.getDay()
-                    ? dueDate.getDate() + dayIndex + (7 - dueDate.getDay())
-                    : dueDate.getDate() + (dayIndex - dueDate.getDay()),
-            );
-            if (/proximo/i.test(day[0])) dueDate.setDate(dueDate.getDate() + 7);
-
-            return dueDate;
-        }
-
-        if (this.prompt.match(exp.es.date4)) {
-            const day = this.prompt.match(exp.es.date2)[0];
-
-            if (/pasado/i.test(day)) dueDate.setDate(dueDate.getDate() + 1);
-            if (/ma.ana/i.test(day)) dueDate.setDate(dueDate.getDate() + 1);
-
-            return dueDate;
-        }
-
-        return false;
+    getDayMonthIndex(dataSource, userString) {
+        return dataSource.findIndex((element) => element === userString);
     }
 
-    getIndex(dataSource, compareTo) {
-        return dataSource.findIndex(
-            (element) => element.slice(0, 2) === compareTo.slice(0, 2),
-        );
+    setNextWeekDay(userDayIndex, baseDate) {
+        return userDayIndex < baseDate.getDay()
+            ? baseDate.getDate() + userDayIndex + (7 - baseDate.getDay())
+            : baseDate.getDate() + (userDayIndex - baseDate.getDay());
     }
 
-    nextRecurrentDate() {
-        if (!exp.es.loopAbsolute.test(this.prompt)) return false;
+    setRelevance(regexResult) {
+        /\*|^i/i.test(regexResult.pop())
+            ? (this.important = true)
+            : (this.urgent = true);
+    }
 
+    setTimer(regexResult) {
+        const units = /h/i.test(regexResult[3]) ? 3600 : 60;
+        this.timer = +regexResult[2] * units;
+        this.timerPast = 0;
+    }
+
+    setNextRecurrentDate(regexResult) {
         const nextDue = new Date();
-        const userDate = this.prompt.match(exp.es.loopAbsolute);
 
-        if (!/\d/.test(userDate[0])) {
-            const dayIndex = this.getIndex(daysWeek.es, userDate[1]);
-
-            nextDue.setDate(
-                dayIndex < nextDue.getDay()
-                    ? nextDue.getDate() + dayIndex + (7 - nextDue.getDay())
-                    : nextDue.getDate() + (dayIndex - nextDue.getDay()),
-            );
+        if (!/\d/.test(regexResult[0])) {
+            const dayIndex = this.getDayMonthIndex(daysWeek.es, regexResult[1]);
+            nextDue.setDate(this.setNextWeekDay(dayIndex, nextDue));
         } else {
-            const lastDayMonth = new Date(
+            const lastDateMonth = new Date(
                 nextDue.getFullYear(),
                 nextDue.getMonth() + 1,
                 0,
             ).getDate();
 
             nextDue.setDate(
-                +userDate[1] > lastDayMonth ? lastDayMonth : +userDate[1],
+                +regexResult[1] > lastDateMonth
+                    ? lastDateMonth
+                    : +regexResult[1],
             );
             nextDue.setMonth(
                 nextDue.getDate() < new Date().getDate()
@@ -167,15 +102,76 @@ class Task {
             );
         }
 
-        return nextDue;
+        this.recurrent = true;
+        this.dueDate = nextDue;
     }
+
+    setFixedDate1(regexResult) {
+        const dueDate = new Date();
+
+        const day = +regexResult[1];
+        let month = regexResult[2].replace(/ de |\//g, '');
+
+        month = /\d/.test(month)
+            ? +month - 1
+            : this.getDayMonthIndex(months.es, month);
+        dueDate.setDate(day);
+        dueDate.setMonth(month);
+        if (new Date() > dueDate)
+            dueDate.setFullYear(dueDate.getFullYear() + 1);
+
+        this.dueDate = dueDate;
+    }
+
+    setFixedDate2(regexResult) {
+        const dueDate = new Date();
+        const day = regexResult;
+        console.log(regexResult)
+
+        let daysBetween = day[5][0] === 'd' ? 1 : 7
+        daysBetween = +daysBetween * +day[0].match(/\d+/);
+
+        if (day[5][0] === 'd') daysBetween -= 1;
+        if (/ma.ana|tomorrow/i.test(day[2])) daysBetween += +1;
+        if (/de e/i.test(day)) {
+            const dayIndex = this.getDayMonthIndex(daysWeek.es, day[3]);
+            dueDate.setDate(this.setNextWeekDay(dayIndex, dueDate));
+        }
+        dueDate.setDate(dueDate.getDate() + daysBetween);
+
+        this.dueDate = dueDate;
+    }
+
+    // setFixedDate3(regexResult) {
+    //     const dueDate = new Date();
+    //     const day = regexResult;
+    //     const dayIndex = this.getDayMonthIndex(daysWeek.es, day[2]);
+    //     nextDue.setDate(this.setNextWeekDay(dayIndex, nextDue));
+    //     if (/proximo/i.test(day[0])) dueDate.setDate(dueDate.getDate() + 7);
+    //
+    //     this.dueDate = nextDue;
+    // }
+    //
+    // setFixedDate4(regexResult) {
+    //     const dueDate = new Date();
+    //     const day = regexResult[0];
+    //
+    //     if (/pasado/i.test(day)) dueDate.setDate(dueDate.getDate() + 1);
+    //     if (/ma.ana/i.test(day)) dueDate.setDate(dueDate.getDate() + 1);
+    //
+    //     this.dueDate = nextDue;
+    // }
 
     readTask() {
         console.log(JSON.stringify(this, null, 2));
     }
 }
 
-const as = new Task(window.prompt('qué quieres hacer?'))
-// const as = new Task('@vida #casa de mañana en 2 semanas  sacar la basura *');
+// const as = new Task(window.prompt('qué quieres hacer?'))
+const as = new Task('* @ # todos en de mañana en 8 dias de noviembre hora carajo');
 
 as.readTask();
+// const test = `@vida #casa de mañana en 2 semanas  sacar la basura *`.match(
+//     exp.es.date2,
+// );
+// console.log(typeof test, test);
