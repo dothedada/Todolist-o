1a4cdd7951fd66081ff17fb26818b72b0e24e382
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-03-10 11:33:27 -0500

    Crea el regex para mail y url

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,7 @@
 const exp = {
     project: /@[a-zA-ZÀ-ÿ]+/,
     category: /#[a-zA-ZÀ-ÿ]+/g,
+    mailOrUrl: /([a-z0-9\-|.]+@[a-z0-9\-]+\.[a-z0-9\-.]+)|((?:https?:\/\/)?(?:[a-z0-9\-]+\.)?([a-z0-9\-]{1,63}\.[a-z0-9]+)(?:\/[\/\w\d\-\+\.\*$%?#]+)?)/ig,
 
     // prettier-ignore
     es: {
@@ -10,7 +11,6 @@ const exp = {
 	loopRelative: /cada ([0-9]{1,2}) (dia|semana|mes)/i,
 	loopPeriod: /durante (enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre|([0-9]{1,2}) mes(es)?) |([0-9]) veces/i,
 
-        // WARN: revisar si la abreviación de los meses puede causar algún error
         date1: /([0-9]{1,2})(\/[0-9]{1,2}| de (enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre))/i,
         date2: /(dentro de |de (hoy|ma.ana|este (lunes|martes|miercoles|jueves|viernes|sabado|domingo)) en |en )([0-9]+ )(dia.|semana.)/i,
 	date3: /(el |este |el proximo )(lunes|martes|miercoles|jueves|viernes|sabado|domingo)/i,
@@ -19,8 +19,22 @@ const exp = {
         week: /lunes|martes|miercoles|jueves|viernes|sabado|domingo/i,
         months: /enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre/i,
     },
-    
-    en: {},
+
+    en: {
+        //        relevance: /[*!]\B|(important|urgent)\b/gi,
+        //        timer: /(t:|tengo) ?([0-9.,]+) ?(minutos?|min|m|horas?|h)\b/i,
+        // loopAbsolute: /todos los (lunes|martes|mi.rcoles|jueves|viernes|s.bado|domingo|[0-9]{1,2})/i,
+        // loopRelative: /cada ([0-9]{1,2}) (dia|semana|mes)/i,
+        // loopPeriod: /durante (enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre|([0-9]{1,2}) mes(es)?) |([0-9]) veces/i,
+        //
+        //        date1: /([0-9]{1,2})(\/[0-9]{1,2}| de (enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre))/i,
+        //        date2: /(dentro de |de (hoy|ma.ana|este (lunes|martes|miercoles|jueves|viernes|sabado|domingo)) en |en )([0-9]+ )(dia.|semana.)/i,
+        // date3: /(el |este |el proximo )(lunes|martes|miercoles|jueves|viernes|sabado|domingo)/i,
+        //        date4: /(pasado )?(hoy|ma.ana)/i,
+        //
+        //        week: /lunes|martes|miercoles|jueves|viernes|sabado|domingo/i,
+        //        months: /enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre/i,
+    },
 };
 
 const daysWeek = {
@@ -33,7 +47,15 @@ const daysWeek = {
         'viernes',
         'sabado',
     ],
-    en: [],
+    en: [
+        'sunday',
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'fryday',
+        'saturday',
+    ],
 };
 const months = {
     es: [
@@ -50,7 +72,20 @@ const months = {
         'noviembre',
         'diciembre',
     ],
-    en: [],
+    en: [
+        'january',
+        'february',
+        'march',
+        'april',
+        'may',
+        'june',
+        'july',
+        'august',
+        'september',
+        'october',
+        'november',
+        'december',
+    ],
 };
 
 export { exp, daysWeek, months };
