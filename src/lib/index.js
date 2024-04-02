d372820dd62cb6b07ee741b54b480da29229d2db
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-04-02 14:59:43 -0500

    actualiza todo

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,15 @@
 import Task from './task.js';
 // TODO:
-// 2- Crear la biblioteca de tareas
-// 3- creación
-// 4- eliminar
-// 5- actualizar
-// 6- filtrar
-// 7- ordenar
+// Crear la biblioteca de tareas con las siguientes posibilidades
+// 1- crear tareas
+// 2- eliminar tareas
+// 3- marcar como tareas realizadas
+// 4- actualizar tareas
+// 5- filtrar tareas
+// 6- ordenar tareas
+// 7- convertir tareas vencidas en urgentes
+// 8- eliminar tareas no cumplidas luego de una semana de su vencimiento
+//
 // WARN: revisar el day cuando se establece un periodo de recurrecia futuro
 
 const as = new Task('@hola carenalga, 12/3 esta es una www.prueba.com')
