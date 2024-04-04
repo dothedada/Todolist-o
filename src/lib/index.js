13fd03b887e5f22d0a7262f4548aa9cce2034f38
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-04-04 15:06:01 -0500

    Reestrura el manejo de las tareas con librerias de funciones en objetos e incorpora la carga de tareas del local storage

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,83 +5,68 @@ import Task from './task.js';
 //
 // WARN: revisar el day cuando se establece un periodo de recurrecia futuro
 
-const toDo = (() => {
-    const tasks = [];
+const toDo = {
+    tasks: [],
 
-    const createTask = (taskPrompt) => {
+    // CRUD
+    createTask: (taskPrompt) => {
         const newTask = new Task(taskPrompt);
-        tasks.push(newTask);
+        toDo.tasks.push(newTask);
         // localStorage.setItem(newTask.taskID, newTask)
-    };
+    },
 
-    const getTaskIndex = (taskID) =>
-        tasks.findIndex((task) => task.taskID === taskID);
+    getTask: (taskID) => toDo.tasks.find((task) => task.taskID === taskID),
 
-    const updateTask = (taskID, newTaskPrompt) => {
-        tasks[getTaskIndex(taskID)].update(newTaskPrompt);
-        // localStorage.setItem(taskID, tasks[getTaskIndex(taskID)])
-    };
+    updateTask: (taskID, newPrompt) => {
+        toDo.getTask(taskID).update(newPrompt);
+        // localStorage.setItem(taskID, toDo.getTask(taskID))
+    },
 
-    const markTaskDone = (taskID) => {
-        tasks[getTaskIndex(taskID)].markDone();
-        // localStorage.setItem(taskID, tasks[getTaskIndex(taskID)])
-    };
-    const markTaskUndone = (taskID) => {
-        tasks[getTaskIndex(taskID)].markUndone();
-        // localStorage.setItem(taskID, tasks[getTaskIndex(taskID)])
-    };
-    const removeTask = (taskID) => {
-        tasks.splice(getTaskIndex(taskID), 1);
+    markTaskDone: (taskID) => {
+        toDo.getTask(taskID).markDone();
+        // localStorage.setItem(taskID, toDo.getTask(taskID))
+    },
+    markTaskUndone: (taskID) => {
+        toDo.getTask(taskID).markUndone();
+        // localStorage.setItem(taskID, toDo.getTask(taskID))
+    },
+    removeTask: (taskID) => {
+        toDo.tasks.splice(
+            toDo.tasks.findIndex((task) => task.taskID === taskID),
+            1,
+        );
         // localStorage.removeItem(taskID)
-    };
+    },
 
-    return {
-        tasks,
-        createTask,
-        removeTask,
-        markTaskDone,
-        markTaskUndone,
-        updateTask,
-    };
-})();
+    // List by
+    anytimeTasks: () => toDo.tasks.filter((task) => !task.dueDate),
 
-const getTasksByDay = (() => {
-    const anytime = () => toDo.tasks.filter((task) => !task.dueDate);
-
-    const today = () =>
+    todayTasks: () =>
         toDo.tasks.filter((task) => {
             if (!task.dueDate) return false;
             return task.dueDate.getDate() === new Date().getDate();
-        });
+        }),
 
-    const tomorrow = () =>
+    tomorrowTasks: () =>
         toDo.tasks.filter((task) => {
             if (!task.dueDate) return false;
             return task.dueDate.getDate() === new Date().getDate() + 1;
-        });
+        }),
 
-    const future = () =>
+    futureTasks: () =>
         toDo.tasks.filter((task) => {
             if (!task.dueDate) return false;
             return task.dueDate.getDate() > new Date().getDate() + 1;
-        });
+        }),
 
-    const pastDue = () =>
+    pastDueTasks: () =>
         toDo.tasks.filter((task) => {
             if (!task.dueDate || task.done) return false;
             return task.dueDate < new Date().setMilliseconds(0);
-        });
+        }),
+};
 
-    return {
-        anytime,
-        today,
-        tomorrow,
-        future,
-        pastDue,
-    };
-})();
-
-const tasksFilteredBy = {
+const filteredBy = {
     importance: (tasks) => tasks.filter((task) => task.important),
     urgent: (tasks) => tasks.filter((task) => task.urgent),
     hasTimer: (tasks) => tasks.filter((task) => task.timer),
@@ -90,47 +75,57 @@ const tasksFilteredBy = {
     recurrent: (tasks) => tasks.filter((task) => task.recurrent),
 };
 
+const globalOperations = {
+    load: () => {
+        const tasksIDs = Object.keys(localStorage).filter((id) =>
+            /^toDo_/.test(id),
+        );
+        tasksIDs.forEach(task => toDo.tasks.push(task))
+    },
 
-const makeDueTasksUrgent = () => {
-    getTasksByDay.pastDue().forEach((task) => {
-        const modifiedTask = task;
-        modifiedTask.urgent = true;
-    });
-};
+    makeDueTasksUrgent: () => {
+        toDo.pastDue().forEach((task) => {
+            const modifiedTask = task;
+            modifiedTask.urgent = true;
+        });
+    },
 
-const deleteTasksForgotten = () => {
-    const monthAgo = new Date();
-    monthAgo.setMilliseconds(0);
-    monthAgo.setMonth(new Date().getMonth() - 1);
+    deleteTasksForgotten: () => {
+        const monthAgo = new Date();
+        monthAgo.setMilliseconds(0);
+        monthAgo.setMonth(new Date().getMonth() - 1);
 
-    const tasksForgotten = toDo.tasks.filter((task) => {
-        if (!task.dueDate || task.done) return false;
-        return task.dueDate < monthAgo;
-    });
+        const tasksForgotten = toDo.tasks.filter((task) => {
+            if (!task.dueDate || task.done) return false;
+            return task.dueDate < monthAgo;
+        });
 
-    tasksForgotten.forEach((task) => toDo.removeTask(task.taskID));
-};
+        tasksForgotten.forEach((task) => toDo.removeTask(task.taskID));
+    },
 
-const deleteDoneTasks = () => {
-    const weekAgo = new Date();
-    weekAgo.setMilliseconds(0);
-    weekAgo.setDate(new Date().getDate() - 7);
+    deleteDoneTasks: () => {
+        const weekAgo = new Date();
+        weekAgo.setMilliseconds(0);
+        weekAgo.setDate(new Date().getDate() - 7);
 
-    const doneTasks = toDo.tasks.filter((task) => {
-        if (!task.done) return false;
-        return task.doneDate < weekAgo;
-    });
+        const doneTasks = toDo.tasks.filter((task) => {
+            if (!task.done) return false;
+            return task.doneDate < weekAgo;
+        });
 
-    doneTasks.forEach((task) => toDo.removeTask(task.taskID));
-};
+        doneTasks.forEach((task) => toDo.removeTask(task.taskID));
+    },
 
-const purgeToDo = () => {
-    deleteDoneTasks();
-    deleteTasksForgotten();
-    makeDueTasksUrgent();
+    purge: () => {
+        globalOperations.deleteDoneTasks();
+        globalOperations.deleteTasksForgotten();
+        globalOperations.makeDueTasksUrgent();
+    },
 };
 
 toDo.createTask('hoy vamos a saltar lazo');
 toDo.createTask('mañana vamos a saltar * por la ventana @carajo');
 toDo.createTask('tratemos de no t:3h morir 20/3');
-console.log(tasksFilteredBy.importance(toDo.tasks))
+console.log(toDo.tasks);
+toDo.markTaskDone(2);
+console.log(toDo.tasks);
